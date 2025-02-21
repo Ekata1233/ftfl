46cdd50894d4016ab5e5fc14b35a9fe15379f922
Ekata1233 <jadhavjanki8462@gmail.com>
2025-02-21 15:05:50 +0530

    job desc ready

--- src/Components/JobDescriptions/JobDescCantact.jsx
+++ src/Components/JobDescriptions/JobDescCantact.jsx
@@ -3,6 +3,7 @@ import { FiUpload } from "react-icons/fi";
 import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import '../../Components/CarreerOpportunities/CarreersOpportunities.css';
+import '../JobDescriptions/jobdescr.css'
 
 function JobDescCantact() {
  const [file, setFile] = useState(null);
--- src/Components/JobDescriptions/JobDescriptions.jsx
+++ src/Components/JobDescriptions/JobDescriptions.jsx
@@ -3,7 +3,7 @@ import HeaderNav from "../HeaderNav/HederNav"
 import carrers from '../../assets/Rectangle 3847.png'
 
 import { useEffect } from "react"
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import JobDescCantact from "./JobDescCantact";
 function JobDescriptions() {
     useEffect(() => {
@@ -50,9 +50,74 @@ function JobDescriptions() {
             </div>
             <Container>
                
-                <h1>jobdescription</h1>
-               
-                
+                <h1>Descriptions</h1>
+             <div>
+             <p className="" style={{fontSize:"28px", fontWeight:"600"}}>Job Description</p>
+             <p className="text-secondary text">As a Senior UX Designer, you will be responsible for leading the user experience strategy for our digital products. You will play a crucial role in understanding user needs, designing intuitive workflows, and ensuring that our products provide a seamless, efficient, and enjoyable experience for users. You will collaborate closely with product managers, UI designers, developers, and stakeholders to create wireframes, prototypes, and user flows that enhance the usability and functionality of our apps and websites.</p>
+             </div>
+             <div>
+             <p className="" style={{fontSize:"28px", fontWeight:"600"}}>Requirements</p>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             </div>
+             <div>
+             <p className="" style={{fontSize:"28px", fontWeight:"600"}}>Work Environment & Expectations</p>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             </div>
+             
+
+             <div>
+             <p className="" style={{fontSize:"28px", fontWeight:"600"}}>Benefits</p>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             <li className="text-secondary text">Lorem ipsum dolor sit amet consectetur.</li>
+             </div>
+
+             <Row className="pt-5">
+                <Col>
+                    <p >Salary (INR)</p>
+                    <p className="text  ">6.5 LPA</p>
+                    <p className="text-secondary text">Yearly salary</p>
+                </Col>
+                <Col>
+                    <p>Job Location</p>
+                    <p className="text-secondary text">Hadapsar, Pune</p>
+                </Col>
+             </Row>
+             <Row className="mt-5 pt-5">
+                <Col>
+                    <p>Job Posted:</p>
+                    <p className="text-secondary text">13 Feb, 2025</p>
+                </Col>
+                <Col>
+                    <p>Job expire in:</p>
+                    <p className="text-secondary text">13 April, 2025</p>
+                </Col>
+                <Col>
+                    <p>Job Level:</p>
+                    <p className="text-secondary text">Entry Level</p>
+                </Col>
+                <Col>
+                    <p>Experience</p>
+                    <p className="text-secondary text">5+ Year</p>
+                </Col>
+                <Col>
+                    <p>Education</p>
+                    <p className="text-secondary text">Graduation</p>
+                </Col>
+             </Row>
                 <JobDescCantact/>
             </Container>
           
